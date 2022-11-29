ceb42783850338c6ae8edbc406fa5d2b5bb38c71
nf1973 <neil.fensom@gmail.com>
2022-11-29 20:02:44 +0100

    fixed issue 1

--- scripts/extras.js
+++ scripts/extras.js
@@ -1,16 +1,20 @@
 async function flashButton(color, timer) {
     switch(color) {
      case "yellow":
-         yellowButton.style.backgroundColor="yellow";
-         break;
+        yellowButton.style.backgroundColor="yellow";
+        beep(200,700-40,30); 
+        break;
      case "red":
-         redButton.style.backgroundColor="red";
-         break;
+        redButton.style.backgroundColor="red";   
+        beep(200,560-36,30);  
+        break;
      case "blue":
-         blueButton.style.backgroundColor="blue";
-         break;
-     case "green":
-         greenButton.style.backgroundColor="green";
+        blueButton.style.backgroundColor="blue";
+        beep(200,350-20,30);  
+        break;
+     case "green":    
+        greenButton.style.backgroundColor="green";
+        beep(200,400-8,30); 
          break;
      default:
        // will never happen
@@ -49,3 +53,51 @@ async function flashButton(color, timer) {
  const delayres = (delayInms) => {
     return new Promise(resolve => setTimeout(resolve, delayInms));
   }
+
+
+
+
+/**
+ * Helper function to emit a beep sound in the browser using the Web Audio API.
+ * 
+ * @param {number} duration - The duration of the beep sound in milliseconds.
+ * @param {number} frequency - The frequency of the beep sound.
+ * @param {number} volume - The volume of the beep sound.
+ * 
+ * @returns {Promise} - A promise that resolves when the beep sound is finished.
+ */
+function beep(duration, frequency, volume){
+    return new Promise((resolve, reject) => {
+        // Set default duration if not provided
+        duration = duration || 200;
+        frequency = frequency || 287;
+        volume = volume || 30;
+
+        try{
+            let oscillatorNode = myAudioContext.createOscillator();
+            let gainNode = myAudioContext.createGain();
+            oscillatorNode.connect(gainNode);
+
+            // Set the oscillator frequency in hertz
+            oscillatorNode.frequency.value = frequency;
+
+            // Set the type of oscillator
+            oscillatorNode.type= "square";
+            gainNode.connect(myAudioContext.destination);
+
+            // Set the gain to the volume
+            gainNode.gain.value = volume * 0.01;
+
+            // Start audio with the desired duration
+            oscillatorNode.start(myAudioContext.currentTime);
+            oscillatorNode.stop(myAudioContext.currentTime + duration * 0.001);
+
+            // Resolve the promise when the sound is finished
+            oscillatorNode.onended = () => {
+                resolve();
+            };
+        }catch(error){
+            reject(error);
+        }
+    });
+}
--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,29 @@ const blueButton  = document.querySelector('#blue-button');
 const greenButton = document.querySelector('#green-button'); 
 var startButton = document.getElementById('start-button');
 const startButtonCss = document.querySelector('#start-button');
+const myAudioContext = new AudioContext();
 
-let flashTime=700;
-let gapBetween=flashTime+200; 
+let flashTime=500;
+let gapBetween=flashTime+100; 
 let gapBeforeNextRound=1000;
 
+let gameIsRunning=false;
 let readyToPlay=false;
-let userSequence = [];
-let sequence = [];
-var round = 0;
+var userSequence = [];
+var sequence = [];
+var thisRound = 0;
+
+function startButtonClicked() {
+    if (!gameIsRunning) {
+        startGame();
+    }
+} 
+
 
 async function startGame() {
+
+    gameIsRunning=true;
+
     //Set Inital Colors only if they are not already set
     if (sequence.length==0) {
         for (i=0; i<3; i++) {
@@ -23,14 +35,12 @@ async function startGame() {
         }
     }
 
-    //round = sequence.length;
-
     startButton.innerHTML="Watch Sequence ("+sequence.length+")";
     startButtonCss.style.backgroundColor="#4281a4";
 
     let delay = await delayres(gapBeforeNextRound); 
 
-    console.log(sequence);
+    //console.log(sequence);  //Uncomment to invoke 'Cheat mode'!!
 
     for (flashes of sequence) {
         flashButton(flashes,flashTime)
@@ -44,7 +54,7 @@ function yellowClicked() {
     if (readyToPlay) { //if not readyToPlay then ignore the click!
         userSequence.push("yellow");
         flashButton("yellow",200);
-        checkSequence(round, "yellow");
+        checkSequence(userSequence.length, "yellow");
     }
 }
 
@@ -52,7 +62,7 @@ function redClicked() {
     if (readyToPlay) { //if not readyToPlay then ignore the click!
         userSequence.push("red");
         flashButton("red",200);
-        checkSequence(round, "red");
+        checkSequence(userSequence.length, "red");
     }
 }
 
@@ -60,7 +70,7 @@ function blueClicked() {
     if (readyToPlay) { //if not readyToPlay then ignore the click!
         userSequence.push("blue");
         flashButton("blue",200);
-        checkSequence(round, "blue");
+        checkSequence(userSequence.length, "blue");
     }
 }
 
@@ -68,43 +78,37 @@ function greenClicked() {
     if (readyToPlay) { //if not readyToPlay then ignore the click!
         userSequence.push("green");
         flashButton("green",200);
-        checkSequence(round, "green");
+        checkSequence(userSequence.length, "green");
     }
 }
 
-function checkSequence(round, colorClicked) {
-    // for (let i = 0; i < (userSequence.length); i++) {
-    //     console.log(i,sequence);
-    //     if (userSequence[i]!=sequence[i]) {
-    //     gameOver();
-    //    }
-    //    else if (userSequence.length==sequence.length) {
-    //     startButton.innerHTML="Watch Sequence";
-    //     userSequence.length=0;
-    //     readyToPlay = false;
-    //     addToSequence(sequence);
-    //     startGame();
-    //  }
-    //  }
-    console.log (round,colorClicked,sequence[round]);
-    if (colorClicked == sequence[round]) {
-        console.log("Correct");
+function checkSequence(thisRound, colorClicked) {
+
+    if (colorClicked != sequence[thisRound-1]) { //reduce by 1 because it has already been incremented
+        gameOver(sequence.length-1);
+        
+    }
+    else if (userSequence.length==sequence.length) {
+        startButton.innerHTML="Watch Sequence";
+        userSequence.length=0;
+        readyToPlay = false;
+        addToSequence(sequence);
+        startGame();
+    }
+}
+
+function gameOver(moves) {
+    if (moves >= 3) {
+        startButton.innerHTML="Game Over ("+moves+")";
     }
     else {
-        console.log("False");
+        startButton.innerHTML="Game Over";
     }
-    round++;
-
-    
-}
-
-function gameOver() {
-    startButton.innerHTML="Game Over";
+    gameIsRunning=false;
     userSequence.length=0;
     sequence.length=0;
     readyToPlay = false;
     startButtonCss.style.backgroundColor="red";
-
 }
 
 
